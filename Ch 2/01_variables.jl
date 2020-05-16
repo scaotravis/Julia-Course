69b7eebb02c90c2bd3ef5233f70e19dfbadebd01
Travis Cao <scaotravis@gmail.com>
2020-05-16 16:23:37 -0500

    ch 2

--- Ch 2/01_variables.jl
+++ Ch 2/01_variables.jl
@@ -7,30 +7,31 @@
 # is too long to be easily read.
 first = 1
 second = 2.0
-stringvar = "This is a string"
+stringVar = "This is a string"
 initial = 'J'
-boolval = true
+boolVal = true
 
 println(first)
 println(second)
-println(stringvar)
+println(stringVar)
 println(initial)
-println(boolval)
+println(boolVal)
 
-# TODO: variables can be redefined and change type
+# Variables can be redefined and change type
 
 
-# TODO: Strings have to be in double quotes. This will error:
+# Strings have to be in double quotes. This will error:
 # anotherstring = 'hello world'
 
-# TODO: Constant values are declared with const and are all uppercase
+# Constant values are declared with const and are all uppercase
 # Note: this is only useful in the global scope
 
 
-# TODO: Constants of the same type *can* be reassigned, but with a warning
+# Constants of the same type *can* be reassigned, but with a warning
 
 
-# TODO: Constants of different types cannot be reassigned, this is an error
+# Constants of different types cannot be reassigned, this is an error
 
 
 # Type Annotation identifies a variable as a particular type
+
