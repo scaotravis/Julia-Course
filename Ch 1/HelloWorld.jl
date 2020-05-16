3c6a35b822a351dc7c761f7ebb095556239c828e
Travis Cao <scaotravis@gmail.com>
2020-05-16 12:51:42 -0500

    ch 1

--- Ch 1/HelloWorld.jl
+++ Ch 1/HelloWorld.jl
@@ -1,21 +1,23 @@
 # Start version of HelloWorld for Learning Julia course
 
-# TODO: Define a function that prints out a message
+# Define a function that prints out a message
 # This creates a function with a single "method"
+function helloWorld()
+    println("Hello world!")
+end
 
-
-# TODO: By defining another version of the function with different
+# By defining another version of the function with different
 # arguments, we create another "method" - Julia will choose
 # the right one based on the arguments. This is called 
 # "multiple dispatch"
+function helloWorld(count::Int)
+    for i in 1:count
+        println("Hello world ", i, "!")
+    end
+end
 
+# Call the version with no arguments
+helloWorld()
 
-# TODO: call the version with no arguments
-
-
-# TODO: call the version that takes a number
-
-
-# TODO: try calling the function with a string - this will error
-# because we have not defined a method that takes a string
-
+# Call the version that takes a number
+helloWorld(3)
