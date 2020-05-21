2028db41290c36d766397cb1d85ae5ad2bc52035
Travis Cao <scaotravis@gmail.com>
2020-05-21 11:18:55 -0500

    ch 5

--- Ch 5/03_sets.jl
+++ Ch 5/03_sets.jl
@@ -4,25 +4,39 @@
 # but a set can only contain one of each element and the ordering
 # of elements is not important
 
-# TODO: create a new empty set
+# create a new empty set
+set1 = Set()
+push!(set1, "a", 2, 5.0)
+println(set1, "\n")
 
-
-# TODO: create a set from a collection of values
-
+# create a set from a collection of values
+set2 = Set([1, 2, 3, 4, 2, 5, 7, 1])
+println(set2, "\n")
 
 # When Julia determines a set type, you can't add different types
+# ex. push!(set2, "abc") will result in an error because "abc" is a String instead of Int64
 
-
-# TODO: create a set of a given type
-
+# create a set of a given type
+rainbow = Set{String}()
+push!(rainbow, "red", "orange", "yellow", "green", "blue", "indigo", "violet")
+println(rainbow, "\n")
 
 # many of the array operations also work with sets
-# TODO: use the in operator to see if a set contains an item
+# use the in operator to see if a set contains an item
+println("red" in rainbow, "\n")
 
+# however, since there's no order, something like an index causes an error
+# ex. println(rainbow[1]) will result in an error
 
-# TODO: however, since there's no order, something like an index causes an error
+# you can calculate set intersections, differences, and unions
+primaryColors = Set(["red", "yellow", "green", "blue"])
+pastels = Set(["teal", "pink", "lavender", "coral"])
 
+commonColors = intersect(rainbow, primaryColors)
+println(commonColors, "\n")
 
-# TODO: you can calculate set intersections, differences, and unions
-primarycolors = Set(["red","yellow","green","blue"])
-pastels = Set(["teal","pink","lavender","coral"])
+allColors = union(rainbow, pastels)
+println(allColors, "\n")
+
+diffColors = setdiff(rainbow, primaryColors)
+println(diffColors)
