fcacb7cb6aaf59ed7e9e2e0b4cc5adc5fda4213a
Travis Cao <scaotravis@gmail.com>
2020-05-21 11:43:09 -0500

    ch 6

--- Ch 6/01_random.jl
+++ Ch 6/01_random.jl
@@ -1,20 +1,33 @@
 # Starting random numbers example for Learning Julia
 using Random
 
-# TODO: generate a random number between 0 and 1
+# generate a random number between 0 and 1
+println(rand(), "\n")
 
+# pick a random element from a given collection
+for i in 1:3
+    println(rand([1, 2, 3, 4, 5, 6])) # choosing 3 random items from the specified array
+end
+println()
 
-# TODO: pick a random element from a given collection
+# populate an array with random values
+arr = rand(UInt8, 5) # populate an array with 5 unsigned Int8
+println(arr, "\n")
 
+# generate a random string
+println(randstring("ABCD")) # default: generate a size 8 random string 
+                            # selected from the specified string
+println(randstring('a':'z', 6))
+println()
 
-# TODO: populate an array with random values
+# shuffle a list of elements
+vowels = ["A", "E", "I", "O", "U"]
+Random.shuffle!(vowels)
+println(vowels, "\n")
 
-
-# TODO: generate a random string
-
-
-# TODO: shuffle a list of elements
-vowels = ["A","E","I","O","U"]
-
-
-# TODO: use the seed function to control the random number generator
+# use the seed function to control the random number generator
+Random.seed!(1000)
+println(rand(), ", ", rand())
+println("--------")
+Random.seed!(1000)
+println(rand(), ", ", rand())
