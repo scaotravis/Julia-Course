8138acd6b8716abc882c2fcfd4d3517223bbaa16
Travis Cao <scaotravis@gmail.com>
2020-05-21 11:07:27 -0500

    ch 5

--- Ch 5/02_tuples.jl
+++ Ch 5/02_tuples.jl
@@ -1,20 +1,30 @@
 # Starting point for tuples example for Learning Julia
 
-# TODO: a tuple is a sequence of values and is immutable once defined
+# a tuple is a sequence of values and is immutable once defined
 # like an array, it can contain different types
+tup1 = (1, 3.0, "five", 7, '9')
+println("The third element of tup1 is ", tup1[3])
+println("The second to fourth elements of tup1 is ", tup1[2:4], "\n")
 
+# Tuples can be iterated over like arrays
+for i in tup1
+    println(i)
+end
+println()
 
-# TODO: Tuples can be iterated over like arrays
+# The "in" operator can be used to see if a tuple contains a value
+println("Is 3 in tup1? ", 3 in tup1)
+println("Is 4 in tup1? ", 4 in tup1)
+println()
 
+# tuples can have multiple dimensions
+tup2 = ((1, 2), (3, 4), (5, 6))
+println("The third element in tup2 is ", tup2[3])
+println("The second element of the first element of tup2 is ", tup2[1][2], "\n")
 
-# TODO: The "in" operator can be used to see if a tuple contains a value
+# attempting to modify a tuple will produce an error
+# ex. tup1[2] = 11 will result in an error
 
-
-# TODO: tuples can have multiple dimensions
-
-
-# TODO: attempting to modify a tuple will produce an error
-
-
-# TODO: Named Tuples provide a way to associate names with tuple values
-
+# Named Tuples provide a way to associate names with tuple values
+namedTup = (a = 1, b = "two", c = '3')
+println("The element associated with name a in namedTup is ", namedTup.a)
